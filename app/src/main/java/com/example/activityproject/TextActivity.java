ff5c6909547285ed091addaec278305ca97b44f3
MRAndrd <maxxxjarudenko@gmail.com>
2021-08-04 21:13:22 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/activityproject/TextActivity.java
@@ -0,0 +1,30 @@
+package com.example.activityproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void ShowToast(View view) {
+        TextView textView = findViewById(R.id.textView);
+        Toast.makeText(TextActivity.this, textView.getText().toString(), Toast.LENGTH_LONG).show();
+
+    }
+}
